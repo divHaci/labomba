9ae578dddec80beb28e253251f14bc88c4b3fbf5
Luigi Niso <luiginiso04@gmail.com>
2024-02-07 23:43:25 +0100

    Creazione Bomba con moduli e funzione gira

--- js/bomba.js
+++ js/bomba.js
@@ -1,26 +1,38 @@
-
-// Design bomba
-// Aggiunta 12 moduli vuoti + pulsante gira per caricare i moduli avanti e dietro
-// Gestire errori e timer
-
 const numero_moduli = 12;
 const bomba = document.querySelector(".bomba");
 
 var fronte = true;
 
-function creaBomba() {
-    for (let i = 0; i < numero_moduli; i++) {
-        // Creiamo un nuovo container vuoto
-        let container_modulo = document.createElement("div");
-        container_modulo.classList.add("container-modulo");
-        // Aggiungiamo il nuovo container per il modulo alla bomba
-        bomba.appendChild(container_modulo);
-    }
-
-    // Impostiamo i container dietro (metà) non visibili
-    for (let i = numero_moduli/2; i < numero_moduli - 1; i++) {
-        bomba.children[i].classList.add("dietro")
+function gira() {
+    if (fronte) {
+        for (let i = numero_moduli / 2; i < numero_moduli - 1; i++) {
+            bomba.children[i].classList.remove("dietro");
+        }
+        for (let i = 0; i < numero_moduli / 2 - 1; i++) {
+            bomba.children[i].classList.add("dietro");
+        }
+        fronte = false;
+    } else {
+        for (let i = numero_moduli / 2; i < numero_moduli - 1; i++) {
+            bomba.children[i].classList.add("dietro");
+        }
+        for (let i = 0; i < numero_moduli / 2 - 1; i++) {
+            bomba.children[i].classList.remove("dietro");
+        }
+        fronte = true;
     }
 }
 
-creaBomba()
+function creaBomba() {
+    for (let i = 0; i < numero_moduli; i++) {
+        let container_modulo = document.createElement("div");
+        container_modulo.classList.add("container-modulo");
+        bomba.appendChild(container_modulo);
+    }
+
+    for (let i = numero_moduli / 2 - 1; i < numero_moduli - 1; i++) {
+        bomba.children[i].classList.add("dietro");
+    }
+}
+
+creaBomba();
