c2647b9a57e920f98b38596e070eb19b51a9a0fd
Steven Molle <steven.molle@linksmt.it>
2024-02-12 01:55:07 +0100

    aggiunge immagini e capitali

---
+++ js/bomba/bandiere.js
@@ -0,0 +1,29 @@
+let nazioni = ["afghanistan", "albania", "algeria",
+    "andorra", "angola", "antiguaebarbuda", "arabiasaudita", "argentina", "armenia", "australia", "austria",
+    "azerbaigian", "bahamas", "bahrein", "bangladesh", "barbaods", "belgio", "belize", "benin", "bhutan", "bielorussia", "bolivia",
+    "bosnia", "botswana", "brasile", "brunel", "bulgaria", "burkinafaso", "burundi", "cambogia", "camerun", "canada", "capoverde",
+    "ciad", "cile", "cina", "cipro", "cittadelvaticano", "colombia", "comore", "congo", "coreadelnord", "coreadelsud", "costadavorio",
+    "costarica", "croazia", "cuba", "danimara", "djubouti", "dominica", "ecuador", "egitto", "elsalvador", "emiratiarabi", "eritrea",
+    "estonia", "etiopia", "figi", "filippine", "finlandia", "francia", "gabon", "galles", "gambia", "georgia", "germania",
+    "ghana", "giappone", "giordania", "grecia", "grenada", "guatamela", "guinea", "guyana", "haiti", "honduras", "hongkong",
+    "india", "indonesia", "inghilterra", "iran", "iraq", "irlanda", "islanda", "isolemarshall", "isolesolomone", "israele", "italia",
+    "jamaica", "kazakistan", "kenya", "kirghizistan", "kiribati", "kuwait", "laos", "lesotho", "lettonia", "libano", "liberia",
+    "libia", "liecthnstein", "lituania", "lussemburgo", "macedonia", "madagascar", "malawi", "maldive", "malesia", "mali", "malta"
+];
+
+let capitali = [
+    "Kabul", "Tirana", "Algeri",
+    "Andorra la Vella", "Luanda", "Saint John's", "Riyad", "Buenos Aires", "Erevan", "Canberra", "Vienna",
+    "Baku", "Nassau", "Manama", "Dacca", "Bridgetown", "Bruxelles", "Belmopan", "Porto Novo", "Thimphu", "Minsk", "Sucre",
+    "Sarajevo", "Gaborone", "Brasilia", "Bandar Seri Begawan", "Sofia", "Ouagadougou", "Bujumbura", "Phnom Penh", "Yaoundé", "Ottawa", "Praia",
+    "N'Djamena", "Santiago", "Pechino", "Nicosia", "Città del Vaticano", "Bogotà", "Moroni", "Kinshasa", "Pyongyang", "Seul", "Yamoussoukro",
+    "San Jose", "Zagabria", "La Habana", "Copenaghen", "Gibuti", "Roseau", "Quito", "Il Cairo", "San Salvador", "Abu Dhabi", "Asmara",
+    "Tallinn", "Addis Abeba", "Suva", "Manila", "Helsinki", "Parigi", "Libreville", "Cardiff", "Banjul", "Tbilisi", "Berlino",
+    "Accra", "Tokyo", "Amman", "Atene", "Saint George's", "Guatemala City", "Conakry", "Georgetown", "Port-au-Prince", "Tegucigalpa", "Hong Kong",
+    "Nuova Delhi", "Giacarta", "Londra", "Tehran", "Baghdad", "Dublino", "Reykjavik", "Majuro", "Honiara", "Gerusalemme", "Roma",
+    "Kingston", "Nursultan", "Nairobi", "Bishkek", "Tarawa", "Kuwait City", "Vientiane", "Maseru", "Riga", "Beirut", "Monrovia",
+    "Tripoli", "Vaduz", "Vilnius", "Lussemburgo", "Skopje", "Antananarivo", "Lilongwe", "Malé", "Kuala Lumpur", "Bamako", "La Valletta"
+];
+
+// per aggiungere le immagini basta prendere il nome della nazione aggiungere .png
+// le capitali sono nello stesso index delle nazioni, quindi la posizione coincide 
