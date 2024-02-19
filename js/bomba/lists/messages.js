4ef194c885f00bd49fbcb671a2acb07f6021439d
Luigi Niso <luiginiso04@gmail.com>
2024-02-19 12:14:54 +0100

    iniziato disturbatore messaggi

---
+++ js/bomba/lists/messages.js
@@ -0,0 +1,26 @@
+var messages = [
+  {
+    message: "Quando c'è il re fai più spect",
+    type: "ban",
+  },
+  {
+    message: "Quando c'è il re fai più spect",
+    type: "ban",
+  },
+  {
+    message: "n***a",
+    type: "ban",
+  },
+  {
+    message: "Chi mi gifta na sub?",
+    type: "ban",
+  },
+  {
+    message: "Forleo vive",
+    type: "ban",
+  },
+  {
+    message: "w la gasa",
+    type: "ban",
+  },
+];
