b9dd9846fe091d6621857973a07e6a035d28abd4
Luigi Niso <luiginiso04@gmail.com>
2024-02-29 18:31:37 +0100

    fix

--- js/loading.js
+++ js/loading.js
@@ -2,8 +2,9 @@ document.addEventListener("DOMContentLoaded", function () {
   console.log("suca");
   // Hide the loading animation
   var loader = document.querySelector(".loader");
+  loader.style.display = "none";
 
   // Show the content
   var content = document.querySelector(".content");
-  //   content.style.display = "block";
+  content.style.display = "block";
 });
